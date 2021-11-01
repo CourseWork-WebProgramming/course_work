40b02ee39aee73cfd205466d626c50dc5c544938
vmikhalskyi <vladmikh31@gmail.com>
2021-11-01 10:52:01 +0200

    Add js to month

--- src/js/month.js
+++ src/js/month.js
@@ -8,6 +8,154 @@ import {
 } from "./constants";
 const data = require('./schedule.json');
 
+const popUp = document.getElementById('month-popup');
+
+function addListenersToClose() {
+    const closeMonthPopUp = document.getElementById('month-close-popup');
+    const popUp = document.getElementById('month-popup');
+    closeMonthPopUp.addEventListener('click', ()=> {
+        popUp.style.visibility = 'hidden';
+        popUp.style.opacity = 0;
+        document.getElementsByClassName("header")[0].classList.remove("disable");
+        document.getElementsByClassName("nav")[0].classList.remove("disable");
+        document.getElementsByClassName("month__container")[0].classList.remove("disable");
+        const currentActiveDay = document.getElementsByClassName("month__markup__day--chosen")[0];
+        if (currentActiveDay) {
+            currentActiveDay.classList.remove("month__markup__day--chosen");
+        }
+        localStorage.setItem("activeDay", "");
+    })
+}
+
+
+function createMobileMonthDay() {
+    popUp.style.visibility = 'visible';
+    popUp.style.opacity = 1;
+    document.getElementsByClassName("header")[0].classList.add("disable");
+    document.getElementsByClassName("nav")[0].classList.add("disable");
+    document.getElementsByClassName("month__container")[0].classList.add("disable");
+    const activeDay = localStorage.getItem("activeDay");
+    const activeMonth = localStorage.getItem("currentMonth");
+    const activeDate = new Date(2021, activeMonth - 1, activeDay);
+    const scheduleContainer = document.getElementById("month-popup");
+    scheduleContainer.innerHTML = "";
+    const currentGroup = localStorage.getItem("group");
+    const monthSchedule = data[currentGroup][activeMonth];
+    const daySchedule = monthSchedule.find(day => day.id === Number(activeDay));
+    const title = document.createElement("div");
+    title.className = "month__popup__title";
+    title.innerHTML = `
+        <p class="month__popup__date">
+            ${activeDay} ${monthsCase[activeMonth]}
+        </p>
+        <p class="month__popup__weekday">
+            ${dayCase[activeDate.getDay()]}
+        </p>`;
+
+    scheduleContainer.appendChild(title);
+
+    const monthDiv = document.createElement("div");
+    monthDiv.className = "month__popup__container";
+
+    if (!daySchedule) {
+        const container = document.createElement("div");
+        container.className = "month__popup__class__container--empty";
+        container.innerHTML = "<p>Здається, сьогодні пар немає...</p><p>Відпочивай!</p>";
+        monthDiv.appendChild(container);
+    } else {
+        const subgroupsContainer = document.createElement("div");
+        subgroupsContainer.className = "month__popup__class__subgroup__container";
+        subgroupsContainer.innerHTML = `
+            <div class="month__popup__class__subgroups">
+            <p class="month__popup__class__subgroup">1 підгрупа</p>
+            <p class="month__popup__class__subgroup">2 підгрупа</p>
+            </div>`;
+    
+        monthDiv.appendChild(subgroupsContainer);
+
+        const lessons = daySchedule.lessons.map((lesson, index) => {
+            const time = lessonsTime[lesson.time].split("-");
+
+            let classes = "";
+
+            if (lesson.isEmpty) {
+                classes = '<div class="month__popup__class__full"></div>';
+
+            } else if (lesson.isSplitted) {
+                classes = `
+                    <div class="month__popup__class month__popup__class--sub ${lesson.first.isEmpty ? 'month__popup__class--empty' : ''}">
+                        ${lesson.first.isEmpty
+                            ? `
+                            <div class="month__popup__class__empty"></div>`
+                            : `
+                            <div class="month__popup__class__title">
+                                <p class="month__popup__class__name">${lesson.first.name}</p>
+                                <p class="month__popup__class__type">${lessonsType[lesson.first.type]}</p>
+                            </div>
+                            <p class="month__popup__class__professor">${lesson.first.professor}</p>
+                            ${lesson.first.isOnline 
+                                ? `<a class="month__popup__class__place" href="${lesson.first.place}">Посилання ></a>`
+                                : `<p class="month__popup__class__place">${lesson.first.place}</p>`
+                            }
+                        `}
+                    </div>
+                    <div class="month__popup__class__divider"></div>
+                    <div class="month__popup__class month__popup__class--sub ${lesson.second.isEmpty ? 'month__popup__class--empty' : ''}">
+                        ${lesson.second.isEmpty
+                            ? `
+                            <div class="month__popup__class__empty"></div>`
+                            : `
+                            <div class="month__popup__class__title">
+                                <p class="month__popup__class__name">${lesson.second.name}</p>
+                                <p class="month__popup__class__type">${lessonsType[lesson.second.type]}</p>
+                            </div>
+                            <p class="month__popup__class__professor">${lesson.second.professor}</p>
+                            ${lesson.second.isOnline 
+                                ? `<a class="month__popup__class__place" href="${lesson.second.place}">Посилання ></a>`
+                                : `<p class="month__popup__class__place">${lesson.second.place}</p>`
+                            }
+                        `}
+                    </div>
+                    `;
+
+            } else {
+                classes = `
+                    <div class="month__popup__class">
+                        <div class="month__popup__class__title">
+                            <p class="month__popup__class__name">${lesson.general.name}</p>
+                            <p class="month__popup__class__type">${lessonsType[lesson.general.type]}</p>
+                        </div>
+                        <p class="month__popup__class__professor">${lesson.general.professor}</p>
+                        ${lesson.general.isOnline 
+                            ? `<a class="month__popup__class__place" href="${lesson.general.place}">Посилання ></a>`
+                            : `<p class="month__popup__class__place">${lesson.general.place}</p>`
+                        }
+                    </div>
+                `;
+            }
+
+            return `
+                <div class="month__popup__time"><p>${time[0]}</p><p>-</p><p>${time[1]}</p></div>
+                <div class="month__popup__class__group ${index % 2 != 0 ? "month__popup__class__group--red" : ""}">
+                    ${classes}
+                </div>`;
+        });
+
+        for (let y = 0; y < lessons.length; y++) {
+            const container = document.createElement("div");
+            container.className = "month__popup__class__container";
+            container.innerHTML = lessons[y];
+
+            monthDiv.appendChild(container);
+        }
+    }
+
+    scheduleContainer.appendChild(monthDiv);
+    scheduleContainer.innerHTML += '<button class="month__popup__close-button" id="month-close-popup">&#10006;</button>';
+    addListenersToClose();
+}
+
+
 function createMonthDay(){
     const activeDay = localStorage.getItem("activeDay");
     const activeMonth = localStorage.getItem("currentMonth");
@@ -151,7 +299,11 @@ function setActiveDay(day) {
     const activeDay = document.getElementById(`month__markup__day__${day}`);
     activeDay.classList.add("month__markup__day--chosen");
     localStorage.setItem("activeDay", day);
-    createMonthDay();
+    if (window.innerWidth > 860) {
+        createMonthDay();
+    } else {
+        createMobileMonthDay();
+    }
 }
 
 function changeActiveDay(event) {
@@ -256,5 +408,7 @@ localStorage.setItem("group", "mit-21");
 const currentDay = new Date();
 const currentMonthId = currentDay.getMonth() + 1;
 createMonthMarkup(currentMonthId);
-setActiveDay(currentDay.getDate());
-createMonthDay();
+if (window.innerWidth > 860) {
+    setActiveDay(currentDay.getDate());
+    createMonthDay();
+} 
