371a30404100fe722093de177381f5aef89ca325
vmikhalskyi <vladmikh31@gmail.com>
2021-11-14 20:16:06 +0200

    Add angular html/scss

---
+++ src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { NavComponent } from './nav/nav.component';
+import { ScheduleComponent } from './schedule/schedule.component';
+import { TableComponent } from './table/table.component';
+import { CalendarComponent } from './calendar/calendar.component';
+import { GroupPopupComponent } from './group-popup/group-popup.component';
+import { NoGroupComponent } from './no-group/no-group.component';
+import { TodayComponent } from './today/today.component';
+import { WeekComponent } from './week/week.component';
+import { MonthComponent } from './month/month.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    NavComponent,
+    ScheduleComponent,
+    TableComponent,
+    CalendarComponent,
+    GroupPopupComponent,
+    NoGroupComponent,
+    TodayComponent,
+    WeekComponent,
+    MonthComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/group-popup/group-popup.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-group-popup',
+  templateUrl: './group-popup.component.html',
+  styleUrls: ['./group-popup.component.scss']
+})
+export class GroupPopupComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
